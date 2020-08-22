92648510e04d26785c429e2b0ae946fcc483d499
Iarly Lana Lins <me@iarly.dev>
2020-08-22 00:28:39 -0300

    Implementing scenario that says the chat should ask the user's nickname when they connect

---
+++ Chat.Server.Domain/ChatFacade.cs
@@ -0,0 +1,17 @@
+﻿using Chat.Server.Domain.Delegates;
+using System;
+using System.Threading.Tasks;
+
+namespace Chat.Server.Domain
+{
+	public class ChatFacade
+	{
+		public event RequestNicknameDelegate OnRequestNickname;
+
+		public Task ConnectAsync(Guid theConnectionUidOfConnectedClient)
+		{
+			OnRequestNickname.Invoke(theConnectionUidOfConnectedClient);
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ Chat.Server.Domain/Delegates/RequestNicknameDelegate.cs
@@ -0,0 +1,7 @@
+﻿using Chat.Server.Domain.Messages;
+using System;
+
+namespace Chat.Server.Domain.Delegates
+{
+	public delegate void RequestNicknameDelegate(Guid connectionUid);
+}
