bd3bb786692605e694c4fcd2126c6a876e262f5c
Iarly Lana Lins <me@iarly.dev>
2020-08-22 01:16:56 -0300

    Implemeting scenario when the user update the nickname for the first time and they connect to the general room automaticaly

---
+++ Chat.Server.Domain/Delegates/UserConnectsAtRoomDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Chat.Server.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Server.Domain.Delegates
+{
+	public delegate void UserConnectsAtRoomDelegate(Guid connectionUid, Client client);
+}
