d9ecb05ef0ffbde0ebcba3f21abc1d27193d755c
Iarly Lana Lins <me@iarly.dev>
2020-08-22 18:11:58 -0300

    Implementing in memory data storage

---
+++ Chat.Server.Data.InMemory/ClientRepository.cs
@@ -0,0 +1,47 @@
+﻿using Chat.Server.Domain.Entities;
+using Chat.Server.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Server.Data.InMemory
+{
+	public class ClientRepository : IClientRepository
+	{
+		public List<Client> Dataset = new List<Client>();
+
+		public Task<Client> FindByNicknameAsync(string nickname)
+		{
+			return Task.FromResult(Dataset.FirstOrDefault(client => client.Nickname == nickname));
+		}
+
+		public Task<IEnumerable<Client>> GetAllClientInTheRoomAsync(string room)
+		{
+			return Task.FromResult(Dataset.Where(client => client.Room == room));
+		}
+
+		public Task<Client> GetByUidAsync(Guid theConnectionUid)
+		{
+			return Task.FromResult(Dataset.FirstOrDefault(client => client.ConnectionUid == theConnectionUid));
+		}
+
+		public Task StoreAsync(Client client)
+		{
+			Dataset.Add(client);
+			return Task.CompletedTask;
+		}
+
+		public async Task UpdateAsync(Client storedClient)
+		{
+			var existentClient = await GetByUidAsync(storedClient.ConnectionUid);
+			
+			if (existentClient != null)
+			{
+				Dataset.Remove(existentClient);
+			}
+
+			await StoreAsync(storedClient);
+		}
+	}
+}
