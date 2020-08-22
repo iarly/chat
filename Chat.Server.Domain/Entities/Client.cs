81233353e5e39892774c1b7fd8646a10265e3904
Iarly Lana Lins <me@iarly.dev>
2020-08-22 12:51:49 -0300

    Adding ConnectionUid to Client entity

--- Chat.Server.Domain/Entities/Client.cs
+++ Chat.Server.Domain/Entities/Client.cs
@@ -1,7 +1,10 @@
-﻿namespace Chat.Server.Domain.Entities
+﻿using System;
+
+namespace Chat.Server.Domain.Entities
 {
 	public class Client
 	{
+		public Guid ConnectionUid { get; set; }
 		public string Nickname { get; set; }
 		public string Room { get; set; }
 	}
