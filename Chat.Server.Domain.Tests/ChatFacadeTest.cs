9f2d791850179091b21733d0261cea80c6426719
Iarly Lana Lins <me@iarly.dev>
2020-08-22 00:38:33 -0300

    Implementing scenario that says the chat should store the client's information when they connect

--- Chat.Server.Domain.Tests/ChatFacadeTest.cs
+++ Chat.Server.Domain.Tests/ChatFacadeTest.cs
@@ -1,4 +1,8 @@
+using Chat.Server.Domain.Entities;
+using Chat.Server.Domain.Factories;
 using Chat.Server.Domain.Messages;
+using Chat.Server.Domain.Repositories;
+using Moq;
 using NUnit.Framework;
 using System;
 using System.Threading.Tasks;
@@ -7,12 +11,18 @@ namespace Chat.Server.Domain.Tests
 {
 	public class ChatFacadeTest
 	{
+		Mock<IClientFactory> ClientFactoryMock;
+
+		Mock<IClientRepository> ClientRepositoryMock;
+
 		ChatFacade ChatFacade;
 
 		[SetUp]
 		public void Setup()
 		{
-			ChatFacade = new ChatFacade();
+			ClientFactoryMock = new Mock<IClientFactory>();
+			ClientRepositoryMock = new Mock<IClientRepository>();
+			ChatFacade = new ChatFacade(ClientRepositoryMock.Object);
 		}
 
 		[Test]
@@ -32,6 +42,22 @@ namespace Chat.Server.Domain.Tests
 
 			// assert
 			Assert.AreEqual(theConnectionUidOfConnectedClient, theConnectionUidFromEvent);
+		}
+
+		[Test]
+		public async Task Should_Store_The_Client_Information_When_Connect()
+		{
+			// arrage
+			Guid theConnectionUidOfConnectedClient = Guid.NewGuid();
+			Client client = new Client();
+
+			ClientFactoryMock.Setup(mock => mock.Create(theConnectionUidOfConnectedClient)).Returns(client);
+
+			// act
+			await ChatFacade.ConnectAsync(theConnectionUidOfConnectedClient);
+
+			// assert
+			ClientRepositoryMock.Verify(mock => mock.Store(client), Times.Once);
 		}
 	}
 }
