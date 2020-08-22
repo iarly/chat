395490e56b86d3698328ad3f3a9d836d5422f113
Iarly Lana Lins <me@iarly.dev>
2020-08-22 16:45:11 -0300

    Creating dummy message broker

---
+++ Chat.Server.MessageBroker.Local/MessageBroker.cs
@@ -0,0 +1,31 @@
+﻿using Chat.Server.Application.Contracts;
+using Chat.Server.Domain.Commands;
+using Chat.Server.MessageBroker.Delegates;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Chat.Server.MessageBroker.Local
+{
+	public class MessageBroker : IMessageBroker
+	{
+		protected static IDictionary<Guid, BrokerSendCommandDelegate> Delegates = new Dictionary<Guid, BrokerSendCommandDelegate>();
+
+		public Task PublishAsync(Command command)
+		{
+			if (Delegates.TryGetValue(command.ConnectionUid, out BrokerSendCommandDelegate onBrokerSendCommandDelegate))
+			{
+				onBrokerSendCommandDelegate.Invoke(command);
+			}
+
+			return Task.CompletedTask;
+		}
+
+		public Task SubscribeAsync(Guid connectionUid, BrokerSendCommandDelegate onReceiveCommand)
+		{
+			Delegates[connectionUid] = onReceiveCommand;
+
+			return Task.CompletedTask;
+		}
+	}
+}
