71d0a8e1c10364a77f402ddac7cf8bd9e1de562d
Iarly Lana Lins <me@iarly.dev>
2020-08-22 12:32:13 -0300

    Implemeting ConnectCommandHandler

---
+++ Chat.Server.Domain/Commands/Handlers/CommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace Chat.Server.Domain.Commands.Handlers
+{
+	public abstract class CommandHandler<TCommand> : ICommandHandler
+	{
+		public Task ProcessAsync(Command command)
+		{
+			if (command is TCommand myCommand)
+			{
+				return InternalProcessAsync(myCommand);
+			}
+
+			return null;
+		}
+
+		protected abstract Task InternalProcessAsync(TCommand command);
+	}
+}
---
+++ Chat.Server.Domain/Commands/Handlers/ConnectCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chat.Server.Domain.Commands.Handlers
+{
+	public class ConnectCommandHandler : CommandHandler<ConnectCommand>
+	{
+		public ConnectCommandHandler(IChatService chatService)
+		{
+			ChatService = chatService;
+		}
+
+		public IChatService ChatService { get; }
+
+		protected override async Task InternalProcessAsync(ConnectCommand command)
+		{
+			await ChatService.ConnectAsync(command.ConnectionUid);
+		}
+	}
+}
