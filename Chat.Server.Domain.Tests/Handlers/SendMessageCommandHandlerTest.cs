8321529037303ed282bd19eabf8e1d2207434776
Iarly Lana Lins <me@iarly.dev>
2020-08-22 12:39:49 -0300

    Creating scenario for SendMessageCommandHandler

---
+++ Chat.Server.Domain.Tests/Handlers/SendMessageCommandHandlerTest.cs
@@ -0,0 +1,82 @@
+﻿using Chat.Server.Domain.Commands;
+using Chat.Server.Domain.Commands.Handlers;
+using Chat.Server.Domain.Entities;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Server.Domain.Tests.Handlers
+{
+	public class SendMessageCommandHandlerTest
+	{
+		Mock<IChatService> ChatServiceMock;
+		SendMessageCommandHandler ConnectCommandHandler;
+
+		[SetUp]
+		public void SetUp()
+		{
+			ChatServiceMock = new Mock<IChatService>();
+			ConnectCommandHandler = new SendMessageCommandHandler(ChatServiceMock.Object);
+		}
+
+		[Test]
+		public void Should_SendPublicMessage_When_Message_Is_Public_And_Not_Targeted()
+		{
+			var connectionUid = Guid.NewGuid();
+			var messageContent = Mock.Of<IMessageContent>();
+
+			var command = new SendMessageCommand()
+			{
+				ConnectionUid = connectionUid,				
+				MessageContent = messageContent,
+				Private = false
+			};
+
+			ConnectCommandHandler.ProcessAsync(command);
+
+			ChatServiceMock.Verify(mock => mock.SendPublicMessageAsync(connectionUid, messageContent));
+		}
+
+		[Test]
+		public void Should_SendPublicMessage_When_Message_Is_Public_And_Targeted()
+		{
+			var targetedClient = "Jefrey";
+			var connectionUid = Guid.NewGuid();
+			var messageContent = Mock.Of<IMessageContent>();
+
+			var command = new SendMessageCommand()
+			{
+				ConnectionUid = connectionUid,
+				MessageContent = messageContent,
+				Private = false,
+				TargetClient = targetedClient
+			};
+
+			ConnectCommandHandler.ProcessAsync(command);
+
+			ChatServiceMock.Verify(mock => mock.SendPublicTargetedMessageAsync(connectionUid, targetedClient, messageContent));
+		}
+
+		[Test]
+		public void Should_SendPrivateMessageAsync_When_Message_Is_Private()
+		{
+			var targetedClient = "Jefrey";
+			var connectionUid = Guid.NewGuid();
+			var messageContent = Mock.Of<IMessageContent>();
+
+			var command = new SendMessageCommand()
+			{
+				ConnectionUid = connectionUid,
+				MessageContent = messageContent,
+				TargetClient = targetedClient,
+				Private = true
+			};
+
+			ConnectCommandHandler.ProcessAsync(command);
+
+			ChatServiceMock.Verify(mock => mock.SendPrivateMessageAsync(connectionUid, targetedClient, messageContent));
+		}
+	}
+}
