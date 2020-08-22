ac921cec30164eb5d58f0e4d2cd2531c6baf7bb3
Iarly Lana Lins <me@iarly.dev>
2020-08-22 18:11:34 -0300

    Implementing IoC

--- Chat.Server.IoC/InjectionConfig.cs
+++ Chat.Server.IoC/InjectionConfig.cs
@@ -1,9 +1,12 @@
 ﻿using Chat.Server.Application;
 using Chat.Server.Application.Contracts;
+using Chat.Server.Data.InMemory;
 using Chat.Server.Domain;
 using Chat.Server.Domain.Entities;
 using Chat.Server.Domain.Factories;
+using Chat.Server.Domain.Repositories;
 using Chat.Server.MessageBroker.Local;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -20,16 +23,19 @@ namespace Chat.Server.IoC
 			return ServiceProvider.GetService<T>();
 		}
 
-		public static void ConfigureServices()
+		public static void ConfigureServices(IConfiguration configuration)
 		{
 			var services = new ServiceCollection();
 
+			services.AddSingleton(configuration);
+			
 			services.AddSingleton<ChatApplication>();
 			
 			services.AddSingleton<IDomainEvents, DomainEvents>();
 
 			services.AddSingleton<IChatFacade, ChatFacade>();
 			services.AddSingleton<IChatService, ChatService>();
+			services.AddSingleton<IClientRepository, ClientRepository>();
 
 			services.AddSingleton<IClientFactory, ClientFactory>();
 			services.AddSingleton<ICommandHandlerFactory, CommandHandlerFactory>();
