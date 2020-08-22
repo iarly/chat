aa2966d9924b6762ebae4ca8be70c3b87331b032
Iarly Lana Lins <me@iarly.dev>
2020-08-22 12:46:44 -0300

    Implemeting SetNicknameCommandHandler

---
+++ Chat.Server.Domain/Commands/Handlers/SetNicknameCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace Chat.Server.Domain.Commands.Handlers
+{
+	public class SetNicknameCommandHandler : CommandHandler<SetNicknameCommand>
+	{
+		public SetNicknameCommandHandler(IChatService chatService)
+		{
+			ChatService = chatService;
+		}
+
+		public IChatService ChatService { get; }
+
+		protected override async Task InternalProcessAsync(SetNicknameCommand command)
+		{
+			await ChatService.UpdateNicknameAsync(command.ConnectionUid, command.Nickname);
+		}
+	}
+}
