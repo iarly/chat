ddd817ce8aac5fce4ac64e2913e48f34be68fdde
Iarly Lana Lins <me@iarly.dev>
2020-08-22 13:01:41 -0300

    Adding IChatService.cs

---
+++ Chat.Server.Domain/Services/IChatService.cs
@@ -0,0 +1,15 @@
+﻿using Chat.Server.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace Chat.Server.Domain
+{
+	public interface IChatService
+	{
+		Task ConnectAsync(Guid theConnectionUidOfConnectedClient);
+		Task SendPrivateMessageAsync(Guid theConnectionUid, string theTargetedUser, IMessageContent theMessageContent);
+		Task SendPublicMessageAsync(Guid theConnectionUid, IMessageContent theMessageContent);
+		Task SendPublicTargetedMessageAsync(Guid theConnectionUid, string theTargetedUser, IMessageContent theMessageContent);
+		Task UpdateNicknameAsync(Guid theConnectionUid, string theNickname);
+	}
+}
