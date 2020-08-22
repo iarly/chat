7256ab16f724cf85ee613508db620aacae6c5636
Iarly Lana Lins <me@iarly.dev>
2020-08-22 12:31:04 -0300

    Creating scenario for ConnectHandler

---
+++ Chat.Server.Domain.Tests/Handlers/ConnectHandlerTest.cs
@@ -0,0 +1,37 @@
+﻿using Chat.Server.Domain.Commands;
+using Chat.Server.Domain.Commands.Handlers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Server.Domain.Tests.Handlers
+{
+	public class ConnectHandlerTest
+	{
+		Mock<IChatService> ChatServiceMock;
+		ConnectCommandHandler ConnectCommandHandler;
+
+		[SetUp]
+		public void SetUp()
+		{
+			ChatServiceMock = new Mock<IChatService>();
+			ConnectCommandHandler = new ConnectCommandHandler(ChatServiceMock.Object);
+		}
+
+		[Test]
+		public void Should_Invoke_Client_Connection()
+		{
+			var connectionUid = Guid.NewGuid();
+			var command = new ConnectCommand()
+			{
+				ConnectionUid = connectionUid
+			};
+
+			ConnectCommandHandler.ProcessAsync(command);
+
+			ChatServiceMock.Verify(mock => mock.ConnectAsync(connectionUid));
+		}
+	}
+}
