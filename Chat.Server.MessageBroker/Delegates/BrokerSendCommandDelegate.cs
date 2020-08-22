6d77cb816ce3cd45859cd135a90b81d31054b715
Iarly Lana Lins <me@iarly.dev>
2020-08-22 16:44:47 -0300

    Creating MessageBroker project

---
+++ Chat.Server.MessageBroker/Delegates/BrokerSendCommandDelegate.cs
@@ -0,0 +1,7 @@
+﻿using Chat.Server.Domain.Commands;
+using System.Threading.Tasks;
+
+namespace Chat.Server.MessageBroker.Delegates
+{
+	public delegate Task BrokerSendCommandDelegate(Command command);
+}
---
+++ Chat.Server.MessageBroker/IMessageBroker.cs
@@ -0,0 +1,14 @@
+﻿using Chat.Server.Domain.Commands;
+using Chat.Server.MessageBroker.Delegates;
+using System;
+using System.Threading.Tasks;
+
+namespace Chat.Server.Application.Contracts
+{
+	public interface IMessageBroker
+	{
+		Task PublishAsync(Command command);
+
+		Task SubscribeAsync(Guid connectionUid, BrokerSendCommandDelegate onReceiveCommand);
+	}
+}
