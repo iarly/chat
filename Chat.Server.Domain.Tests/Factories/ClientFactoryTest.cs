4b9c5953fcea3f86c9f39350a52203a51def6151
Iarly Lana Lins <me@iarly.dev>
2020-08-22 12:51:13 -0300

    Creating scenario for ClientFactory

---
+++ Chat.Server.Domain.Tests/Factories/ClientFactoryTest.cs
@@ -0,0 +1,27 @@
+﻿using Chat.Server.Domain.Factories;
+using NUnit.Framework;
+using System;
+
+namespace Chat.Server.Domain.Tests.Factories
+{
+	public class ClientFactoryTest
+	{
+		IClientFactory ClientFactory;
+
+		[SetUp]
+		public void Setup()
+		{
+			ClientFactory = new ClientFactory();
+		}
+
+		[Test]
+		public void Should_Create_Client()
+		{
+			var connectionUid = Guid.NewGuid();
+
+			var client = ClientFactory.Create(connectionUid);
+
+			Assert.AreEqual(client.ConnectionUid, connectionUid);
+		}
+	}
+}
