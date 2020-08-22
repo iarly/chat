e4784bc33120e441658df251d209c4174045962e
Iarly Lana Lins <me@iarly.dev>
2020-08-22 12:12:27 -0300

    Implemeting scenario for the new ChatFacade when it is processing a command

---
+++ Chat.Server.Domain/ChatFacade.cs
@@ -0,0 +1,24 @@
+﻿using Chat.Server.Domain.Commands;
+using Chat.Server.Domain.Factories;
+using System;
+using System.Threading.Tasks;
+
+namespace Chat.Server.Domain
+{
+	public class ChatFacade
+	{
+		private ICommandHandlerFactory CommandHandlerFactory;
+
+		public ChatFacade(ICommandHandlerFactory commandHandlerFactory)
+		{
+			CommandHandlerFactory = commandHandlerFactory;
+		}
+
+		public async Task ProcessMessageAsync(Guid connectionUid, Command command)
+		{
+			var handler = CommandHandlerFactory.GetHandler(command);
+
+			await handler.ProcessAsync(connectionUid, command);
+		}
+	}
+}
