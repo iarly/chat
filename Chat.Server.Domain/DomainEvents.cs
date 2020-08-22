0f8d8aff5490ff3af6e73b2fea5408d3b35fa855
Iarly Lana Lins <me@iarly.dev>
2020-08-22 11:39:16 -0300

    Implemeting DomainEvents

---
+++ Chat.Server.Domain/DomainEvents.cs
@@ -0,0 +1,43 @@
+﻿using Chat.Server.Domain.Delegates;
+using Chat.Server.Domain.Entities;
+using System;
+
+namespace Chat.Server.Domain
+{
+	public class DomainEvents : IDomainEvents
+	{
+		public event RequestNicknameDelegate OnRequestNickname;
+
+		public event UserConnectsAtRoomDelegate OnUserConnectsAtRoom;
+
+		public event UserSentMessageDelegate OnUserSentMessage;
+
+		public event UserSentPrivateMessageDelegate OnUserSentPrivateMessage;
+
+		public void InvokeOnUserSentMessage(string room, Message message)
+		{
+			OnUserSentMessage.Invoke(room, message);
+		}
+
+		public void InvokeOnUserSentPrivateMessageEvent(Client client, TargetedMessage message)
+		{
+			OnUserSentPrivateMessage.Invoke(client, message);
+		}
+
+		public void InvokeRequestNicknameEvent(Guid theConnectionUidOfConnectedClient)
+		{
+			if (OnRequestNickname != null)
+			{
+				OnRequestNickname.Invoke(theConnectionUidOfConnectedClient);
+			}
+		}
+
+		public void InvokeOnUserConnectsAtRoomEvent(Guid theConnectionUid, Client client)
+		{
+			if (OnUserConnectsAtRoom != null)
+			{
+				OnUserConnectsAtRoom.Invoke(theConnectionUid, client);
+			}
+		}
+	}
+}
---
+++ Chat.Server.Domain/IDomainEvents.cs
@@ -0,0 +1,19 @@
+﻿using Chat.Server.Domain.Delegates;
+using Chat.Server.Domain.Entities;
+using System;
+
+namespace Chat.Server.Domain
+{
+	public interface IDomainEvents
+	{
+		event RequestNicknameDelegate OnRequestNickname;
+		event UserConnectsAtRoomDelegate OnUserConnectsAtRoom;
+		event UserSentMessageDelegate OnUserSentMessage;
+		event UserSentPrivateMessageDelegate OnUserSentPrivateMessage;
+
+		void InvokeOnUserConnectsAtRoomEvent(Guid theConnectionUid, Client client);
+		void InvokeOnUserSentMessage(string room, Message message);
+		void InvokeOnUserSentPrivateMessageEvent(Client client, TargetedMessage message);
+		void InvokeRequestNicknameEvent(Guid theConnectionUidOfConnectedClient);
+	}
+}
