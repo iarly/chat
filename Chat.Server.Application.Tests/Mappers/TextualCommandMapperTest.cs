8fdfaffc5a200fe4b7ea5dadc12b7a5ceae5534b
Iarly Lana Lins <me@iarly.dev>
2020-08-23 11:47:50 -0300

    Creating scenarios for TextualCommandMapper

---
+++ Chat.Server.Application.Tests/Mappers/TextualCommandMapperTest.cs
@@ -0,0 +1,36 @@
+﻿using Chat.Server.Application.Enumerators;
+using Chat.Server.Application.Mappers;
+using Chat.Server.Domain.Commands;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Server.Application.Tests.Mappers
+{
+	public class TextualCommandMapperTest
+	{
+		private TextualCommandMapper Mapper;
+
+		[SetUp]
+		public void Setup()
+		{
+			Mapper = new TextualCommandMapper();
+		}
+
+		[Test]
+		public void Should_Convert_Message_To_SetNicknameCommand_When_WaitingNickaname()
+		{
+			string messageSentByClient = "Joey";
+			ClientState currentStateOfClient = ClientState.WaitingNickname;
+			Guid connectionUid = Guid.NewGuid();
+
+			Command command = Mapper.ToCommand(connectionUid, currentStateOfClient, messageSentByClient);
+
+			Assert.IsAssignableFrom<SetNicknameCommand>(command);
+			Assert.AreEqual(messageSentByClient, ((SetNicknameCommand)command).Nickname);
+			Assert.AreEqual(connectionUid, ((SetNicknameCommand)command).ConnectionUid);
+		}
+
+	}
+}
